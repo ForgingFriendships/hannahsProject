eb7ab60f1f0f245a879c339156d93c48b628f832
saasbook <saasbook@saasbook.(none)>
2015-04-04 22:40:47 -0300

    Installed git OmniAuth

---
+++ app/controllers/home_page_controller.rb
@@ -0,0 +1,9 @@
+class HomePageController < ApplicationController
+  def index
+  	# defaults just renders the page.
+  end
+
+  def blog
+
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Forging::Application.config.secret_token = '417dd31c747a8170683a20cd5608735283c713b351fc4e9856c020da93525b99b6970bd370147e4553dde239d965b769b4749ead78b8954e9292580dd02c2bd7'
