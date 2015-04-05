050421c0a2d5eabe856d6ecbfde140f677713a5b
saasbook <saasbook@saasbook.(none)>
2015-04-05 11:12:31 -0300

    Got facebook login working up to point where session controller tries to make a new user.  Then reach the problem that :uid, :name, :provider attributes of a user (in user model) are protected and cannot be mass-assigned via hash to create a new user.  Is there a way to fix this without simply changing attr_protected to attr_accessible? (Because that would introduce security vulnerability).  Ask professor? -Greg

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ActiveRecord::Base
   attr_protected :name, :provider, :uid
   def self.create_with_omniauth(auth)
-  	User.create!(
-  		:provider => auth["provider"], 
-  		:uid => auth["uid"], 
-  		:name => auth["info"]["name"]
-  		)
+    User.create!(
+      :provider => auth["provider"],
+      :uid => auth["uid"],
+      :name => auth["info"]["name"]
+      )
+  end
 end
---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :facebook, '835976856438515', '77f4bbc0b651b36fb3c4c80d54638a72'
+end
