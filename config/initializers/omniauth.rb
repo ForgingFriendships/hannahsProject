a6274d2697473a105f32b42dc22ae0f3f4502565
Oaggin Park <saasbook@saasbook.(none)>
2015-04-09 23:57:53 -0300

    Added google auth, simple.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '835976856438515', '77f4bbc0b651b36fb3c4c80d54638a72'
   provider :twitter, 'kJE5Awze7kbscrg45u339Gw94', 'me5PLTgkFECxNVdKmY8j40NKXehAkLoZBb69QmOPCBLe5C5xOs'
+  provider :google_oauth2, '559258992679-vvt7dbitdkkhhl79he9hkqklpemc3gpf.apps.googleusercontent.com',
+  '24uCH0aZKDIBm4CzESjoPib6'
 end
