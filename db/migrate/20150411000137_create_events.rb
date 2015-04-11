18583579104f0768256fcebe34fe5dc3785bdae6
Jann Ton <jton@mills.edu>
2015-04-10 21:43:43 -0300

    created new model Events with attributes set up

---
+++ db/migrate/20150411000137_create_events.rb
@@ -0,0 +1,22 @@
+class CreateEvents < ActiveRecord::Migration
+  def change
+    create_table :events do |t|
+      t.string   :event_name
+      t.text  	 :description
+      t.string   :host
+      #:null => false == cannot give a null value
+      t.datetime :start_date_time, :null => false
+      t.datetime :end_date_time, :null => false
+      t.string   :venue
+      t.text     :address
+      t.integer  :num_persons_attending
+      t.integer  :max_capacity
+      t.timestamps
+    end
+  end
+  
+  def down
+    drop_table :events
+  end
+
+end
