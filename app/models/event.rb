f2502a1dfa32ca8f3e179104c5b504d9b39e4a7a
Jann Ton <jton@mills.edu>
2015-04-18 03:42:09 -0300

    greg and i almost got events page work, adding new events is working

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,3 @@
 class Event < ActiveRecord::Base
-  attr_accessible :event_name, :description, :host, :start_date_time, :end_date_time, :venue, :address
+  attr_accessible :event_name, :description, :host, :start_date_time, :end_date_time, :venue, :address, :max_capacity
 end
